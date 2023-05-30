cd06804b0aae9ce933339aad87bd4f8405e5a9af
Jason Rector <jrector@auxiliumgroup.ca>
2023-05-30 14:57:25 -0400

    bump

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # lazy-mans-viewer
 This *was* supposed to be me just putting nothing useful here, but I ended up finding a really easy way to just isolate a webpage and serve it as a single exe. It's simple af, I just think it's neat.
 
-npm install electron --save-dev
-npm install electron-packager --save-dev
-npm install --save-dev electron-builder
+npm install electron
+npm install electron-packager
+npm install electron-builder
+npm install electron-updater
--- main.js
+++ main.js
@@ -1,32 +1,40 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow } = require("electron");
+const { autoUpdater, AppUpdater } = require("electron-updater");
+const path = require("path");
+const { fork } = require("child_process");
+fork(path.join(__dirname, "/main.js"));
 
-const path = require('path');
-const { fork } = require('child_process');
-fork(path.join(__dirname, '/main.js'));
+//Autoupdater
+autoUpdater.autoDownload = true;
+autoUpdater.autoInstallOnAppQuit = true;
 
-function createWindow () {
+function createWindow() {
   const win = new BrowserWindow({
     width: 1024,
     height: 800,
     webPreferences: {
       nodeIntegration: false,
-      contextIsolation: true
-    }
-  })
+      contextIsolation: true,
+    },
+  });
 
-  win.loadURL('https://www.tiktok.com/en/') // replace link
+  win.loadURL("https://www.tiktok.com/en/"); // replace link
 }
 
-app.whenReady().then(createWindow)
+app.whenReady().then(() => {
+  createWindow();
 
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
 
-app.on('activate', () => {
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow()
+  autoUpdater.checkForUpdates();
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
   }
-})
+});
